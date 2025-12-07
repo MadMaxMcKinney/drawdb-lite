bb66b4e9c91571b70bcbf88b24f50a5f7f99eb35
Max McKinney <max@maxmckinney.com>
2025-12-07 11:13:22 -0600

    routing: make Editor the root route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useLayoutEffect } from "react";
 import Editor from "./pages/Editor";
-import BugReport from "./pages/BugReport";
-import Templates from "./pages/Templates";
-import LandingPage from "./pages/LandingPage";
 import SettingsContextProvider from "./context/SettingsContext";
 import NotFound from "./pages/NotFound";
 
@@ -13,10 +10,7 @@ export default function App() {
       <BrowserRouter>
         <RestoreScroll />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/editor" element={<Editor />} />
-          <Route path="/bug-report" element={<BugReport />} />
-          <Route path="/templates" element={<Templates />} />
+          <Route path="/" element={<Editor />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
